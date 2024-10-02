b25bc175abd270049ec11474833bae2ccb78eb34
jcampb98 <caboose768@gmail.com>
2024-10-02 09:33:09 +0100

    applied a small fix

--- src/components/layout/FormAction.tsx
+++ src/components/layout/FormAction.tsx
@@ -1,5 +1,7 @@
+import { FormEvent } from "react";
+
 interface FormActionProps {
-    handleSubmit: () => void;
+    handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
     type?: string;
     action?: 'submit' | 'button' | 'reset';
     text: string;
@@ -18,7 +20,7 @@ export default function FormAction({
             <button
                 type={action}
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-10"
-                onClick={handleSubmit}
+                onClick={(e) => handleSubmit(e as unknown as FormEvent<HTMLFormElement>)}
             >
                 {text}
             </button>
