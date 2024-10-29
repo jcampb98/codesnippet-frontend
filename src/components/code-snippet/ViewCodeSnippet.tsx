ec6ef939f20d290c58bef5558f5e5b24e435f11f
jcampb98 <caboose768@gmail.com>
2024-10-29 11:15:41 +0000

    implemented the copy to clipboard function for the code snippet

--- src/components/code-snippet/ViewCodeSnippet.tsx
+++ src/components/code-snippet/ViewCodeSnippet.tsx
@@ -1,4 +1,5 @@
 import CodeBlock from "../../components/code-snippet/CodeBlock";
+import { toast } from "react-toastify";
 
 interface ViewCodeSnippetProps {
     title: string;
@@ -6,12 +7,18 @@ interface ViewCodeSnippetProps {
 }
 
 const ViewCodeSnippet = ({ title, code_snippet }: ViewCodeSnippetProps)  => {
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(code_snippet);
+
+        toast.success("Code snippet copied to clipboard!");
+    };
+
     return(
         <>
             <h3 className="code-snippet-title">{title}</h3>
             <br />
             <div className="code-block">
-                <button type="button" className="copy-btn">Copy</button>
+                <button type="button" className="copy-btn" onClick={copyToClipboard}>Copy</button>
                 <div className="code-content">
                     <CodeBlock code={code_snippet} />
                 </div>
